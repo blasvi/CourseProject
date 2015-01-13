39f535a8f6ef5b2c4f0435b64f881f12ef938dca
blasvi <Victor_orestes@hotmail.com>
2015-01-13 12:39:56 -0200

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -1,11 +1,11 @@
-run_analysis<- function(){
+run_analysis <- function(){
   features <- read.table("features.txt", header = FALSE,col.name = c("id","operation"))
   names <- as.character(features$operation)
   xtrain <- read.table("X_train.txt", header = FALSE, col.name = names)
   xtest <- read.table("X_test.txt", header = FALSE, col.name = names)
   k <- numeric()
   for(i in 1:561){
-    if(grepl("mean",name[i])|grepl("Mean",name[i])|grepl("std",name[i])){
+    if(grepl("mean",names[i])|grepl("Mean",names[i])|grepl("std",names[i])){
       k = c(k,i)
     }
   }
@@ -15,30 +15,17 @@ run_analysis<- function(){
   x<-rbind(ytrain,ytest)
   for( i in 1:10299){
     if(x[i,1]== 1){
-      x[i,1] = "WALKING"
-    }else{
-      if(x[i,1] == 2){
-        x[i,1] = "WALKING_UPSTAIRS"
-      }else{
-        if(x[i,1] ==3){
-          x[i,1] = "WALKING_DOWNSTAIRS"
-        }else{
-          if(x[i,1] == 4){
-            x[i,1] = "SITTING"
-          }else{
-            if(x[i,1] == 5){
-              x[i,1] = "STANDING"
-            }else{
-              if(x[i,1] == 6){
-                x[i,1] = "LAYING"
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  xalmost <- cbind(x, xtotal)
-  tidy <- aggregate(xalmost[,2:87],by=list(as.factor(xalmost$activity)),mean)
+      x[i,1] = "WALKING"}else{if(x[i,1] == 2){
+x[i,1] = "WALKING_UPSTAIRS"}else{if(x[i,1] ==3){
+x[i,1] = "WALKING_DOWNSTAIRS"}else{if(x[i,1] == 4){
+x[i,1] = "SITTING"}else{if(x[i,1] == 5){
+x[i,1] = "STANDING"}else{if(x[i,1] == 6){
+x[i,1] = "LAYING"
+}}}}}}}
+  subtrain <- read.table("subject_train.txt",header = FALSE, col.name= c("subject"))
+  subtest <- read.table("subject_test.txt",header = FALSE, col.name= c("subject"))
+  xalmost <- cbind(rbind(subtrain,subtest),x, xtotal)
+  tidy <- aggregate(xalmost[,3:87],by=list(xalmost$subject,xalmost$activity),mean)
+  tidy <- rename(tidy, subject = Group.1,activity = Group.2) 
   tidy
 }
